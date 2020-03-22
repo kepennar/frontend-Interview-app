afb7b19fea35d7f9da881e01c0e8cd4d487d6a21
cyr <cyril.quandalle@chanel.com>
2020-03-22 21:54:39 +0100

    [beerRating] - Add sort back to have best scored beers at the top of the list

--- front/src/redux/state/beers/beers.selectors.js
+++ front/src/redux/state/beers/beers.selectors.js
@@ -1,3 +1,8 @@
-export const beerItemsSelector = state => state.beers.items;
+export const beerItemsSelector = state =>
+  state.beers.items.sort((a, b) => {
+    if (a.score < b.score) return 1;
+    if (a.score > b.score) return -1;
+    return 0;
+  });
 
 export const newBeersSelector = state => state.beer.newItems;
