80f94ac3c5bb0eb364eae44719900969e354c970
cyr <cyril.quandalle@chanel.com>
2020-03-22 15:09:52 +0100

    [beerRating] - Remove sort which was making the app a bit jumpy and should have been the reducer role to do it

--- front/src/redux/state/beers/beers.selectors.js
+++ front/src/redux/state/beers/beers.selectors.js
@@ -1,6 +1,1 @@
-export const beerItemsSelector = state =>
-  state.beers.items.sort((a, b) => {
-    if (a.score < b.score) return 1;
-    if (a.score > b.score) return -1;
-    return 0;
-  });
+export const beerItemsSelector = state => state.beers.items;
