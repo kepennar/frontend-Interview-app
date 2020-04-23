350edd65ff995152c6a327d992a67b6dd80acc61
Thomas Esseul <esseul.tho@gmail.com>
2020-04-23 17:59:44 +0200

    Rating première version

--- front/src/components/BeerItem/Rate/Rate.jsx
+++ front/src/components/BeerItem/Rate/Rate.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import styles from "./Rate.module.scss";
 export const Rate = ({ onRate }) => (
   <div className={styles.rate}>
-    <div className={styles.inLove} />
-    <div className={styles.happy} />
-    <div className={styles.sad} />
+    <div className={styles.inLove} onClick={() => onRate(2)} />
+    <div className={styles.happy} onClick={() => onRate(1)} />
+    <div className={styles.sad} onClick={() => onRate(-1)} />
   </div>
 );
--- front/src/components/BeerList/BeerList.component.jsx
+++ front/src/components/BeerList/BeerList.component.jsx
@@ -4,9 +4,9 @@ import { Loader } from "../Loader";
 
 import styles from "./BeerList.module.scss";
 
-export const BeerList = ({ beers, loading }) => (
+export const BeerList = ({ beers, loading, editBeer }) => (
   <div className={styles.Beerlist}>
-    {!loading && beers.map(beer => <BeerItem key={beer.uuid} beer={beer} />)}
+    {!loading && beers.map(beer => <BeerItem key={beer.uuid} beer={beer} onRate={(addscore) => editBeer({ ...beer, score: beer.score + addscore })} />)}
     {loading && <Loader />}
   </div>
 );
